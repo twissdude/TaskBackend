5021af8e7205eed381d6af9b5e2d5ca954868134
twissdude <woyetajudeen@gmail.com>
2024-11-23 23:19:39 +0100

    server corrected

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('./config/db');
-const path = require('path');
+// const path = require('path');
 const taskRoutes = require('./routes/taskRoutes');
 const authRoutes = require('./routes/authRoutes');
 
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', authRoutes);
 
-app.use(express.static(path.join(__dirname, '../Frontend')));
+// app.use(express.static(path.join(__dirname, '../Frontend')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../Frontend', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../Frontend', 'index.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
