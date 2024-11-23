de861391af5b914985c54039c8cb4a214b31f342
twissdude <woyetajudeen@gmail.com>
2024-11-23 22:07:50 +0100

    Initial commit

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
-const { protect } = require('../middleware/authMiddleware');
+// const { protect } = require('../middleware/authMiddleware');
 
-router.post('/', protect, taskController.createTask);
-router.get('/', protect, taskController.getTasks);
-router.put('/:id', protect, taskController.updateTask);
-router.delete('/:id', protect, taskController.deleteTask);
+router.post('/', taskController.createTask);
+router.get('/', taskController.getTasks);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
